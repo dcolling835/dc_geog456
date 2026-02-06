a1644dac907175479a798b4fe18a3edd5a975942
Danielle Colling <dolling@unc.edu>
2026-02-05 21:51:34 -0500

    Create ncgardens.js

---
+++ homework3/ncgardens.js
@@ -0,0 +1,245 @@
+const ncGardens = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Sarah P. Duke Gardens",
+                "city": "Durham",
+                "county": "Durham County",
+                "established": 1934,
+                "acres": 55,
+                "specialty": "Historic terraces, Asiatic Arboretum, and seasonal displays",
+                "admission": "Free"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-78.9382, 36.0014]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "North Carolina Botanical Garden",
+                "city": "Chapel Hill",
+                "county": "Orange County",
+                "established": 1966,
+                "acres": 700,
+                "specialty": "Native Southeastern plants and nature preserve",
+                "admission": "Free"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-79.0353, 35.9032]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Daniel Stowe Botanical Garden",
+                "city": "Belmont",
+                "county": "Gaston County",
+                "established": 1999,
+                "acres": 380,
+                "specialty": "Orchid conservatory and spectacular fountain garden",
+                "admission": "Paid"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-81.0639, 35.2151]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Airlie Gardens",
+                "city": "Wilmington",
+                "county": "New Hanover County",
+                "established": 1901,
+                "acres": 67,
+                "specialty": "Historic azaleas and 467-year-old Airlie Oak",
+                "admission": "Paid"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-77.8228, 34.2399]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Reynolda Gardens",
+                "city": "Winston-Salem",
+                "county": "Forsyth County",
+                "established": 1917,
+                "acres": 129,
+                "specialty": "Historic estate gardens of R.J. Reynolds family",
+                "admission": "Free"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-80.2817, 36.1080]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "JC Raulston Arboretum",
+                "city": "Raleigh",
+                "county": "Wake County",
+                "established": 1976,
+                "acres": 10,
+                "specialty": "Test garden for ornamental plants at NC State",
+                "admission": "Free"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-78.6782, 35.7915]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "North Carolina Arboretum",
+                "city": "Asheville",
+                "county": "Buncombe County",
+                "established": 1986,
+                "acres": 434,
+                "specialty": "Blue Ridge mountain flora and bonsai collection",
+                "admission": "Free (parking fee)"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-82.6219, 35.5011]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Biltmore Estate Gardens",
+                "city": "Asheville",
+                "county": "Buncombe County",
+                "established": 1895,
+                "acres": 8000,
+                "specialty": "Frederick Law Olmsted designed gardens at historic estate",
+                "admission": "Paid"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-82.5515, 35.5406]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Cape Fear Botanical Garden",
+                "city": "Fayetteville",
+                "county": "Cumberland County",
+                "established": 1989,
+                "acres": 80,
+                "specialty": "Native Cape Fear River valley plants",
+                "admission": "Paid"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-78.9536, 35.0382]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Tryon Palace Gardens",
+                "city": "New Bern",
+                "county": "Craven County",
+                "established": 1952,
+                "acres": 14,
+                "specialty": "Colonial-era English gardens at historic capitol",
+                "admission": "Paid"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-77.0441, 35.1085]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Elizabethan Gardens",
+                "city": "Manteo",
+                "county": "Dare County",
+                "established": 1951,
+                "acres": 10,
+                "specialty": "16th-century English garden on Roanoke Island",
+                "admission": "Paid"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-75.7048, 35.9321]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Greensboro Arboretum",
+                "city": "Greensboro",
+                "county": "Guilford County",
+                "established": 1991,
+                "acres": 17,
+                "specialty": "Specialty plant collections in urban setting",
+                "admission": "Free"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-79.8364, 36.0990]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Wing Haven Gardens",
+                "city": "Charlotte",
+                "county": "Mecklenburg County",
+                "established": 1927,
+                "acres": 3,
+                "specialty": "Urban bird sanctuary with formal gardens",
+                "admission": "Free (donations)"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-80.8209, 35.2016]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Coker Arboretum",
+                "city": "Chapel Hill",
+                "county": "Orange County",
+                "established": 1903,
+                "acres": 5,
+                "specialty": "Historic UNC campus arboretum with diverse trees",
+                "admission": "Free"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-79.0514, 35.9101]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Sandhills Horticultural Gardens",
+                "city": "Pinehurst",
+                "county": "Moore County",
+                "established": 1979,
+                "acres": 32,
+                "specialty": "Conifer collection and rose gardens",
+                "admission": "Free"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-79.4583, 35.1954]
+            }
+        }
+    ]
+};
